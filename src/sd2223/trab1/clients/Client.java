6d900d94d9d8aa4bc6612cc4a5ef5ea174957ecc
RafaelMartins4 <rlo.martins@campus.fct.unl.pt>
2023-04-09 17:27:42 +0100

    Added Discovery and Client

---
+++ src/sd2223/trab1/clients/Client.java
@@ -0,0 +1,8 @@
+package sd2223.trab1.clients;
+
+public class Client {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
--- src/sd2223/trab1/server/UsersServer.java
+++ src/sd2223/trab1/server/UsersServer.java
@@ -7,36 +7,40 @@ import java.util.logging.Logger;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import sd2223.trab1.api.Discovery;
 import sd2223.trab1.server.resources.UsersResource;
 
 public class UsersServer {
 
-	private static Logger Log = Logger.getLogger(UsersServer.class.getName());
+    private static Logger Log = Logger.getLogger(UsersServer.class.getName());
 
-	static {
-		System.setProperty("java.net.preferIPv4Stack", "true");
-	}
+    static {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+    }
 
-	public static final int PORT = 8080;
-	public static final String SERVICE = "UsersService";
-	private static final String SERVER_URI_FMT = "http://%s:%s/rest";
+    public static final int PORT = 8080;
+    public static final String SERVICE = "UsersService";
+    private static final String SERVER_URI_FMT = "http://%s:%s/rest";
 
-	public static void main(String[] args) {
-		try {
+    public static void main(String[] args) {
+        try {
 
-			ResourceConfig config = new ResourceConfig();
-			config.register(UsersResource.class);
-			// config.register(CustomLoggingFilter.class);
+            ResourceConfig config = new ResourceConfig();
+            config.register(UsersResource.class);
+            //config.register(CustomLoggingFilter.class);
 
-			String ip = InetAddress.getLocalHost().getHostAddress();
-			String serverURI = String.format(SERVER_URI_FMT, ip, PORT);
-			JdkHttpServerFactory.createHttpServer(URI.create(serverURI), config);
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            String serverURI = String.format(SERVER_URI_FMT, ip, PORT);
+            JdkHttpServerFactory.createHttpServer(URI.create(serverURI), config);
 
-			Log.info(String.format("%s Server ready @ %s\n", SERVICE, serverURI));
+            Discovery disc = Discovery.getInstance();
+            disc.announce(SERVICE, serverURI);
 
-			// More code can be executed here...
-		} catch (Exception e) {
-			Log.severe(e.getMessage());
-		}
-	}
+            Log.info(String.format("%s Server ready @ %s\n", SERVICE, serverURI));
+
+            // More code can be executed here...
+        } catch (Exception e) {
+            Log.severe(e.getMessage());
+        }
+    }
 }
